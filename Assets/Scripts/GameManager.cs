8b842f64be354a4c14d9d12189ab812638e5dca6
Alzawbari Mohammed <38676494+AlzawbariMoes@users.noreply.github.com>
2020-04-24 23:29:37 +0300

    UPDATED THE MOVEMENT CODE

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	
+	public GameObject player;
+	private GameObject currentPlayer;
+	private GameCamera cam;
+	
+	void Start () {
+		cam = GetComponent<GameCamera>();
+		SpawnPlayer(Vector3.zero);
+	}
+	
+	// Spawn player
+	private void SpawnPlayer(Vector3 spawnPos) {
+		currentPlayer = Instantiate(player,spawnPos,Quaternion.identity) as GameObject;
+		cam.SetTarget(currentPlayer.transform);
+	}
+	
+	private void Update() {
+		if (!currentPlayer) {
+			if (Input.GetButtonDown("Respawn")) {
+				SpawnPlayer(Vector3.zero);
+			}
+		}
+	}
+}
---
+++ Assets/og.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class og : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI()
+	{
+		if(GUI.Button(new Rect(400,400,100,50),"play"))
+			
+		{
+			Application.LoadLevel(1);
+			
+		}
+	}
+	
+	
+}
